7cf118bcf44a4d1ad1a8f2e5b6940540972a8996
Nelsen Septa <nelsenseptahenidar@gmail.com>
2022-08-05 19:57:44 +0700

    dashboard 2

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ const Dashboard = () => {
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-200">
       <h1>Dashboard</h1>
+      <p>Contoh Dashboard</p>
     </div>
   );
 };
